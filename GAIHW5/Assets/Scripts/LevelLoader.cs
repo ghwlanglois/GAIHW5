ddd0008fc6c791979a8ff0da08a4d1a9e266d1ce
ghwlanglois <ghwlanglois@gmail.com>
2017-11-06 13:20:09 -0500

    level loader

---
+++ GAIHW5/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+
+public class LevelLoader : MonoBehaviour {
+
+    public GameObject Block;
+    public GameObject Pellet;
+    public string fileName;
+    string[][] grid;
+
+    // Use this for initialization
+    void Start ()
+    {
+        int i = 0;
+        try
+        {
+            string line;
+            StreamReader reader = new StreamReader(fileName, Encoding.Default);
+            using (reader)
+            {
+                do
+                {
+                    line = reader.ReadLine();
+
+                    if (line != null)
+                    {
+                        string[] entries = line.Split();
+                        if (System.Array.IndexOf(entries, "T") != -1 ||
+                            System.Array.IndexOf(entries, "@") != -1 ||
+                            System.Array.IndexOf(entries, ".") != -1)
+                        {
+                            grid[i] = entries;
+                            ++i;
+                        }
+                    }
+                }
+                while (line != null);
+
+                reader.Close();
+            }
+        }
+        catch
+        {
+            Debug.Log("Could not read input file for level loader.");
+        }
+
+        int x = 0, y = 0, j = 0, u = 0;
+
+        for (j = 0; j < i; ++j) 
+        {
+            for (u = 0; u < grid[j].Length; ++u)
+            {
+                if (grid[j][u] == "T")
+                {
+                    Instantiate(Block, new Vector3(x, y, 0), Quaternion.identity);
+                }
+                else if (grid[j][u] == ".")
+                {
+                    Instantiate(Pellet, new Vector3(x, y, 0), Quaternion.identity);
+                }
+                ++x;
+            }
+            ++y;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
