68ddeff07ef24728091bf67b76bee8f905333e1a
Dan Gorman <danthemangorman@gmail.com>
2017-11-06 19:56:35 -0500

    fixed other things

--- GAIHW5/Assets/Scripts/LevelLoader.cs
+++ GAIHW5/Assets/Scripts/LevelLoader.cs
@@ -14,22 +14,22 @@ public class LevelLoader : MonoBehaviour {
     public GameObject[][] TileGrid;
     public int[][] TileStates; //0 = unexplored, 1 = explored, 2 = used
 
-    string[][] grid;
+    char[][] grid;
 
     // Use this for initialization
     void Start ()
     {
         int i = 0;
         foreach (string line in Map.text.Split('\n')){
-
             if (line != null)
             {
-                string[] entries = line.Split();
-                if (System.Array.IndexOf(entries, "T") != -1 ||
-                    System.Array.IndexOf(entries, "@") != -1 ||
-                    System.Array.IndexOf(entries, ".") != -1)
+                char[] entries = line.ToCharArray();
+                if (System.Array.IndexOf(entries, 'T') != -1 ||
+                    System.Array.IndexOf(entries, '@') != -1 ||
+                    System.Array.IndexOf(entries, '.') != -1)
                 {
                     grid[i] = entries;
+                    Debug.Log("Hello");
                     ++i;
                 }
             }
@@ -41,20 +41,17 @@ public class LevelLoader : MonoBehaviour {
         {
             for (; u < grid[j].Length-1; ++u)
             {
-                if (grid[j][u] == "T")
-                {
+                if (grid[j][u] == 'T') {
                     TileGrid[j][uT] = Instantiate(Tree, new Vector3(x, y, 0), Quaternion.identity);
                 }
-                else if (grid[j][u] == "@")
-                {
+                else if (grid[j][u] == '@') {
                     TileGrid[j][uT] = Instantiate(Block, new Vector3(x, y, 0), Quaternion.identity);
                 }
-                else if (grid[j][u] == "." && grid[j][u + 1] == ".")
-                {
+                else if (grid[j][u] == '.' && grid[j][u + 1] == '.') {
                     TileGrid[j][uT] = Instantiate(Tile, new Vector3(x, y, 0), Quaternion.identity);
                     ++u;
                 }
-                else if (grid[j][u] == "." && grid[j][u + 1] != ".")
+                else if (grid[j][u] == '.' && grid[j][u + 1] != '.')
                 {
                     TileGrid[j][uT] = Instantiate(PartialTile, new Vector3(x, y, 0), Quaternion.identity);
                     x -= 0.5f;
