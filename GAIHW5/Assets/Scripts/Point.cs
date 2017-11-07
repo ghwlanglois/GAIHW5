a5eddd47d62d8b7af07bb04a4d739fcf0f65c7f7
Dan Gorman <danthemangorman@gmail.com>
2017-11-06 22:08:21 -0500

    added point class did some other things

---
+++ GAIHW5/Assets/Scripts/Point.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Point{
+
+    int x;
+    int y;
+    char type;
+
+    public int X {
+        get {
+            return x;
+        }
+        set {
+            this.x = value;
+        }
+    }
+
+    public int Y {
+        get {
+            return y;
+        }
+        set {
+            this.y = value;
+        }
+    }
+
+    public char Type {
+        get {
+            return type;
+        }
+        set {
+            type = value;
+        }
+    }
+
+    Point (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // override object.Equals
+    public override bool Equals(object obj) {
+        //       
+        // See the full list of guidelines at
+        //   http://go.microsoft.com/fwlink/?LinkID=85237  
+        // and also the guidance for operator== at
+        //   http://go.microsoft.com/fwlink/?LinkId=85238
+        //
+
+        if (obj == null || GetType() != obj.GetType()) {
+            return false;
+        }
+
+        Point o = (Point)obj;
+        return this.X == o.X && this.Y == o.Y;
+    }
+
+    // override object.GetHashCode
+    public override int GetHashCode() {
+        int tmp = (Y + ((X + 1) / 2));
+        return X + (tmp * tmp);
+    }
+}
